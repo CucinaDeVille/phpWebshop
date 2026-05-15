bea27eebad218cd60777495c5b441ff7adbeeeff
luca <lromano@stud.hs-offenburg.de>
2026-05-15 17:56:50 +0200

    refactor: :art: restructure project

---
+++ public/actions/add_to_cart.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = [];
+}
+
+$id = $_GET['id'] ?? null;
+$name = $_GET['name'] ?? null;
+$price = $_GET['price'] ?? null;
+
+if (!$id || !$name || !$price) {
+    die("Missing product data");
+}
+
+$_SESSION['cart'][] = [
+    "id" => $id,
+    "name" => $name,
+    "price" => $price
+];
+
+header("Location: ../cart.php");
+exit;
