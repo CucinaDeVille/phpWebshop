bcb993ba474ac375a9935c7686a75f9a8bfec8ce
luca <lromano@stud.hs-offenburg.de>
2026-05-15 17:54:29 +0200

    feat(cart): :sparkles: add shopping cart

---
+++ public/cart.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = [];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Shopping cart</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container mt-5">
+
+    <h1>Shopping cart</h1>
+
+    <?php if (empty($_SESSION['cart'])): ?>
+
+        <p>The shopping cart is empty.</p>
+
+    <?php else: ?>
+
+        <ul class="list-group">
+
+            <?php foreach ($_SESSION['cart'] as $index => $product): ?>
+
+                <li class="list-group-item d-flex justify-content-between">
+
+                    <span>
+                        <?= htmlspecialchars($product['name']) ?> - <?= $product['price'] ?> €
+                    </span>
+
+                    <a href="/actions/remove_from_cart.php?index=<?= $index ?>" class="btn btn-sm btn-danger">
+                        Remove
+                    </a>
+
+                </li>
+
+            <?php endforeach; ?>
+
+        </ul>
+
+    <?php endif; ?>
+
+    <a href="index.php" class="btn btn-primary mt-3">
+        Go back
+    </a>
+
+</div>
+
+</body>
+</html>
