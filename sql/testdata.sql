5bc0617af720fc99df80145b45740fc805c8f5d8
luca <lromano@stud.hs-offenburg.de>
2026-05-17 18:05:48 +0200

    fix(data): :bug: update ids on inserted data

--- sql/testdata.sql
+++ sql/testdata.sql
@@ -1,38 +1,38 @@
 INSERT INTO categories (name, parent_id) VALUES
                                              ('Electronics', NULL),
 
-                                             ('Computers', 1),
-                                             ('Phones', 1),
-                                             ('Tablets', 1),
-                                             ('Monitors', 1),
-                                             ('TVs', 1),
-                                             ('Accessories', 1),
+                                             ('Computers', 1), -- id2
+                                             ('Phones', 1), -- id3
+                                             ('Tablets', 1), -- id4
+                                             ('Monitors', 1), -- id5
+                                             ('TVs', 1), -- id6
+                                             ('Accessories', 1), -- id7
 
 -- Computers subcategories
-                                             ('MacBook', 2),
-                                             ('iMac', 2),
-                                             ('Mac Mini', 2),
+                                             ('MacBook', 2), -- id8
+                                             ('iMac', 2), -- id9
+                                             ('Mac Mini', 2), -- id10
 
 -- Phones (direct products will be here)
-                                             ('iPhones', 3),
+                                             ('iPhones', 3), -- id11
 
 -- Tablets
-                                             ('iPads', 4),
+                                             ('iPads', 4), -- id12
 
 -- Monitors
-                                             ('4K Monitors', 5),
-                                             ('5K Monitors', 5),
-                                             ('Ultrawide Monitors', 5),
-                                             ('Studio Display', 5),
+                                             ('4K Monitors', 5), -- id13
+                                             ('5K Monitors', 5), -- id14
+                                             ('Ultrawide Monitors', 5), -- id15
+                                             ('Studio Display', 5), -- id16
 
 -- TVs
-                                             ('4K TVs', 6),
-                                             ('OLED TVs', 6),
+                                             ('4K TVs', 6), -- id17
+                                             ('OLED TVs', 6), -- id18
 
 -- Accessories
-                                             ('AirPods', 7),
-                                             ('Apple Watch', 7),
-                                             ('Chargers & Cables', 7);
+                                             ('AirPods', 7), -- id19
+                                             ('Apple Watch', 7), -- id20
+                                             ('Chargers & Cables', 7); -- id21
 
 INSERT INTO products (name, description, image, price, category_id) VALUES
 
@@ -56,14 +56,16 @@ INSERT INTO products (name, description, image, price, category_id) VALUES
 ('Mac Mini M2', 'Compact desktop computer', 'macmini.jpg', 799.00, 10),
 
 -- Monitors
-('Apple Studio Display', '5K Retina display', 'studiodisplay.jpg', 1599.00, 15),
 ('LG UltraFine 4K', 'High quality 4K monitor for Mac', 'lg4k.jpg', 699.00, 13),
+('LG UltraFine 5K', 'High quality 5K monitor for Mac', 'lg5k.jpg', 1099.00, 14),
+('Samsung UltraWide 4K', '32 inch monitor for productive workflows', 'samsunguw.jpg', 899.00, 15),
+('Apple Studio Display', '5K Retina display', 'studiodisplay.jpg', 1599.00, 16),
 
 -- TVs
-('LG OLED C3 55"', 'OLED TV with perfect blacks', 'oled55.jpg', 1499.00, 17),
-('Samsung 4K Smart TV', 'Ultra HD smart television', 'samsung4k.jpg', 899.00, 16),
+('Samsung 4K Smart TV', 'Ultra HD smart television', 'samsung4k.jpg', 899.00, 17),
+('LG OLED C3 55"', 'OLED TV with perfect blacks', 'oled55.jpg', 1499.00, 18),
 
 -- Accessories
-('AirPods Pro', 'Noise cancelling earbuds', 'airpodspro.jpg', 279.00, 18),
-('Apple Watch Series 9', 'Smartwatch with health tracking', 'watch9.jpg', 499.00, 19);
-
+('AirPods Pro', 'Noise cancelling earbuds', 'airpodspro.jpg', 279.00, 19),
+('Apple Watch Series 9', 'Smartwatch with health tracking', 'watch9.jpg', 499.00, 20);
+('Apple MagSafe Duo', 'Charge iPhone and Apple Watch at the same time', 'magsafeduo.jpg', 89.00, 21);
