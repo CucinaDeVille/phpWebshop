8709f495d550bd3dfeb95189f5510abada5b4503
luca <lromano@stud.hs-offenburg.de>
2026-05-17 15:23:12 +0200

    feat: :construction: connection to mysql database

---
+++ includes/db.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = "db";
+$dbname = "webshop";
+$user = "webshop";
+$pass = "webshop";
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $pass
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+
+    die("DB Fehler: " . $e->getMessage());
+
+}
