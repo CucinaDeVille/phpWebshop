db502093a201c5f3333a2c993a2cd01064beb910
luca <lromano@stud.hs-offenburg.de>
2026-05-17 15:31:03 +0200

    test(database): :bento: import test data into db

---
+++ sql/testdata.sql
@@ -0,0 +1,8 @@
+INSERT INTO categories (name, parent_id)
+VALUES
+    ('Electronics', NULL),
+    ('Clothing', NULL),
+    ('Smartphones', 1),
+    ('Laptops', 1),
+    ('Trousers', 2),
+    ('Shoes', 2);
