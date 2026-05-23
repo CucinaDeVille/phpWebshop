103a960c69a2ade1f714977b27ff417cd8277ab0
luca <lromano@stud.hs-offenburg.de>
2026-05-23 14:53:01 +0200

    fix(registration): :goal_net: catch error for already existing username

--- public/actions/register_action.php
+++ public/actions/register_action.php
@@ -12,6 +12,21 @@ $email = $_POST['email'];
 
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
+$stmt = $pdo->prepare("SELECT id FROM users WHERE username = ?");
+$stmt->execute([$username]);
+
+$existingUser = $stmt->fetch();
+
+if ($existingUser) {
+    echo "
+    <script>
+        alert('Username already taken');
+        window.location.href = '/register.php';
+    </script>
+    ";
+    exit;
+}
+
 $stmt = $pdo->prepare("
     INSERT INTO users
     (username, password, firstname, lastname, address, email)
