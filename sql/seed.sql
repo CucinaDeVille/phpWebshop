6789022ae6bc10d804516294eeead7bdf2134122
luca <lromano@stud.hs-offenburg.de>
2026-05-23 13:44:31 +0200

    feat(database): :seedling: add seed file with initial admin user

---
+++ sql/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO users
+(username, password, firstname, lastname, address, email)
+VALUES
+    (
+        'admin',
+        '$2y$10$Mf/y/dXswg85LyrAIy0qzuqv3bf/JQZwkn.6GloRVs4zoxBMTBF2i',
+        'Admin',
+        'User',
+        'Admin Street 1',
+        'admin@example.com'
+    );
