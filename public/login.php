fbba7647c2801092b4bf02e6125cfc525ed18ad8
luca <lromano@stud.hs-offenburg.de>
2026-05-23 16:39:21 +0200

    refactor: :fire: remove duplicate sign in logic

--- public/login.php
+++ public/login.php
@@ -9,43 +9,6 @@ if (isset($_SESSION['user_id'])) {
 }
 
 $error = "";
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    $username = trim($_POST['username']);
-    $password = trim($_POST['password']);
-
-    $stmt = $pdo->prepare(
-        "SELECT * FROM users WHERE username = ?"
-    );
-
-    $stmt->execute([$username]);
-
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($user && password_verify($password, $user['password'])) {
-
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['username'] = $user['username'];
-        $_SESSION['last_activity'] = time();
-
-        echo "
-            <script>
-                alert('Login successful');
-                window.location.href='index.php';
-            </script>
-        ";
-
-        header("Location: index.php");
-
-        exit;
-
-    } else {
-
-        $error = "Username or password incorrect";
-
-    }
-}
 ?>
 
 <!DOCTYPE html>
