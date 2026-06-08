7b3fcd3519c95968750b4e7ae1a6aa49c9640375
luca <luca.romano@t-online.de>
2026-06-08 14:18:12 +0200

    feat(session): :technologist: improve readability of code

--- includes/session.php
+++ includes/session.php
@@ -2,17 +2,15 @@
 
 session_start();
 
-$timeout = 600;
+$timeout = 600; // 10 minutes
 
 if (isset($_SESSION['last_activity'])) {
 
+    // if more than 10 minutes have passed user is logged out
     if (time() - $_SESSION['last_activity'] > $timeout) {
-
         session_unset();
         session_destroy();
-
     }
-
 }
 
 $_SESSION['last_activity'] = time();
