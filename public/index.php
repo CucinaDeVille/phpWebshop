b0eaaf1c98cc9ac8f09160c10c10e730303011ca
luca <luca.romano@t-online.de>
2026-06-14 12:21:12 +0200

    feat(navbar): :sparkles: add bootstrap carousel

--- public/index.php
+++ public/index.php
@@ -11,6 +11,16 @@ session_start();
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         rel="stylesheet">
+
+    <style>
+        .carousel-inner img {
+            max-height: 400px;
+            width: auto;
+            object-fit: contain;
+            margin: 0 auto;
+        }
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
@@ -55,10 +65,32 @@ session_start();
         Here you can find and buy products!
     </p>
 
-    <a href="actions/add_to_cart.php?id=1"
-       class="btn btn-success">
-        Add to cart
-    </a>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="assets/images/router.jpeg" class="d-block w-100" alt="router">
+            </div>
+            <div class="carousel-item">
+                <img src="assets/images/mouse.jpeg" class="d-block w-100" alt="mouse">
+            </div>
+            <div class="carousel-item">
+                <img src="assets/images/hairdryer.jpeg" class="d-block w-100" alt="hairdryer">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 
     <form action="search.php" method="GET" class="mt-4">
 
@@ -84,6 +116,6 @@ session_start();
     </form>
 
 </div>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
